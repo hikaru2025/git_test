98d01c6b92d0f4a9fbd94a53209f81e1ae02c469
“xie” <hex2025@163.com>
2019-04-19 09:39:58 +0800

    许冠杰

--- src/main/java/git/com/test/controller/TestController.java
+++ src/main/java/git/com/test/controller/TestController.java
@@ -5,4 +5,11 @@ package git.com.test.controller;
  * @date 2019/4/19 9:32
  */
 public class TestController {
+    public static void main(String[] args) {
+        System.out.println("sdad");
+        System.out.println("sdad");
+        System.out.println("史蒂芬孙但是的方式");
+        System.out.println("许冠杰");
+
+    }
 }
---
+++ src/main/java/git/com/test/mapper/Huuuu.java
@@ -0,0 +1,14 @@
+package git.com.test.mapper;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author xiemin
+ * @date 2019/4/19 9:39
+ */
+@Service
+public class Huuuu {
+
+    private String name;
+
+}
